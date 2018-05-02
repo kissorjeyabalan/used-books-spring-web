175e6ae37b3429d759d7b36192758f04f50cc8f6
Kissor Jeyabalan <git@kij.no>
2018-05-02 12:19:27 +0200

    initial commit

---
+++ web/src/main/java/App.java
@@ -0,0 +1,7 @@
+package no.octopod.usedbook;
+
+public class App {
+    public static String getHello() {
+        return "Hello";
+    }
+}
---
+++ web/src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+package no.octopod.usedbook;
+
+import junit.framework.TestCase;
+
+public class AppTest extends TestCase {
+    public void testGetHello() throws Exception {
+        assertEquals("Hello", App.getHello());
+    }
+}
